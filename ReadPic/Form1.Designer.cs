217fcb0aaa0eace63a9b5a9bd643bb2a55711fcc
shareyin <shareyin@qq.com>
2017-08-31 14:33:22 +0800

    识图Demo
    百度识图Demo

---
+++ ReadPic/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ReadPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnOPen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(415, 90);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "识别";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnOPen
+            // 
+            this.btnOPen.Location = new System.Drawing.Point(415, 39);
+            this.btnOPen.Name = "btnOPen";
+            this.btnOPen.Size = new System.Drawing.Size(75, 23);
+            this.btnOPen.TabIndex = 1;
+            this.btnOPen.Text = "打开图片";
+            this.btnOPen.UseVisualStyleBackColor = true;
+            this.btnOPen.Click += new System.EventHandler(this.btnOPen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 263);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOPen);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnOPen;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ReadPic/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReadPic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string willReadPath = "";
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                FormToExcel(textBox1.Text);
+            }
+        }
+        //打开图片路径
+        private void btnOPen_Click(object sender, EventArgs e)
+        {
+            string path = string.Empty;
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+
+            }
+            textBox1.Text = path;
+        }
+        //调用百度识图API
+        public static void FormToExcel(string picPath)
+        {
+            var form = new Baidu.Aip.Ocr.Form("mQgLfOLfbgjSE7NEY0aoruWD", "8js5uXvSqPdDPrKyod1qsXcE8ISjyxDZ");
+            var image = File.ReadAllBytes(picPath);
+            form.DebugLog = true;  // 是否开启调试日志
+
+            // 识别为Excel
+            var result = form.RecognizeToExcel(image);
+            //MessageBox.Show(result);
+            Console.Write(result);
+        }
+    }
+}
